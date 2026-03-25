b473a02dd74eadfe323a7ceec71a697e94fea76d
Adrian Bilescu <diti223@gmail.com>
2026-03-25 11:54:29 +0200

    fix: Update LEMON_APP_ID and LEMON_APP_STORE_URL for correct app identification

--- src/lib/lemon-app.ts
+++ src/lib/lemon-app.ts
@@ -1,2 +1,2 @@
-export const LEMON_APP_ID = '6670605474';
-export const LEMON_APP_STORE_URL = `https://apps.apple.com/app/lemon-nutrition/id${LEMON_APP_ID}`;
+export const LEMON_APP_ID = "6618148167";
+export const LEMON_APP_STORE_URL = `https://apps.apple.com/app/id${LEMON_APP_ID}?platform=iphone`;
